c223a125503d779d58fabf5cf191d691a9549c8c
Mr-Sann <139286702+Mr-Sann@users.noreply.github.com>
2024-10-06 19:33:27 +0100

    Sons
    tentativa de adiconar sons no menu

--- Script/menu.gd
+++ Script/menu.gd
@@ -1,6 +1,5 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Configuração dos botões
 	for button in get_tree().get_nodes_in_group("button"):
@@ -8,9 +7,10 @@ func _ready() -> void:
 
 # Função chamada quando um botão é pressionado
 func on_button_pressed(button: Button) -> void:
+	
 	match button.name:
 		"Play":
-			var _game: bool = get_tree().change_scene_to_file("res://Interface/play1.tscn") 
+			var _game: bool = get_tree().change_scene_to_file("res://Interface/play1.tscn")
 		"Settings":
 			var _controls: bool = get_tree().change_scene_to_file("res://Interface/settings.tscn")
 		"Credits":
