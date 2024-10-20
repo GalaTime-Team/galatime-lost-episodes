d7a3d9fe00e9f507538ec84e7af292751097111c
Mr-Sann <139286702+Mr-Sann@users.noreply.github.com>
2024-10-20 19:58:47 +0100

    Linguagem adicionada no settngs
    adição de linguagem , funcional por enquanto apenas no menu

--- Script/settings.gd
+++ Script/settings.gd
@@ -5,7 +5,9 @@ extends Control
 @onready var back_button = $MarginContainer/Back as Button
 @onready var window_button = $MarginContainer/VBoxContainer/WindowMode/Window_button as OptionButton
 @onready var res_button = $MarginContainer/VBoxContainer/ResolutionMode/Res_button as OptionButton
+@onready var language_button = $MarginContainer/VBoxContainer/HBoxLinguas/language_button as OptionButton
 @onready var b_menu_click = $b_menu_click as AudioStreamPlayer
+@onready var current_language = TranslationServer.get_locale() #traduzir atravez do Excel
 
 const RESOLUTION_MODE_DICTIONARY : Dictionary  = {
 	"1920 x 1080" : Vector2i(1920, 1080),
@@ -28,14 +30,22 @@ const WINDOW_MODE_ARRAY : Array[String] = [
 	"Borderless Window",
 ]
 
+const LANGUAGE_ARRAY : Array[String] = [
+	"Português",
+	"English",
+	"日本語"
+]
+
 signal back_setting_menu
 
 # Função chamada quando a cena entra pela primeira vez
 func _ready() -> void:
 	add_window_mode_items() #os windows mods para conseguirem ser alterados
 	add_res_mode_items() #as resulotions mods para conseguirem ser alteradas
+	add_language_items() #a language para conseguir ser alterada
 	res_button.item_selected.connect(on_res_mode_selected)
 	window_button.item_selected.connect(on_window_mode_selected)
+	language_button.item_selected.connect(on_language_select)
 	back_button.button_down.connect(on_back_pressed)
 	set_process(false) #processo feito para cosneguir voltar ao menu e ter as informações
 
@@ -47,15 +57,14 @@ func on_back_pressed() -> void: #func do processo que ira ser feito no menu
 func add_window_mode_items() -> void: #fazer um loop for pra analizar o array e ver o window mod correto
 	for window_mode in WINDOW_MODE_ARRAY:
 		window_button.add_item(window_mode)
-		
+
 func add_res_mode_items() -> void: #fazer um loop for para analizar todas as resuloções existestes e adicionar a correta
 	for resolution_size_text in RESOLUTION_MODE_DICTIONARY:
 		res_button.add_item(resolution_size_text)
-		
+
 func on_res_mode_selected(index : int) -> void: #para alterar a resulotion mod
 	DisplayServer.window_set_size(RESOLUTION_MODE_DICTIONARY.values()[index])
-			
-	
+
 func on_window_mode_selected(index : int) -> void: #para alterar os windows mods
 	match  index:
 		0: #Window Mode
@@ -67,3 +76,19 @@ func on_window_mode_selected(index : int) -> void: #para alterar os windows mods
 		2: #Borderless window
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+
+func add_language_items() -> void:
+	for language in LANGUAGE_ARRAY:
+		language_button.add_item(language)
+
+func on_language_select(index : int) -> void:
+	match index:
+		0: #Português
+			TranslationServer.set_locale("pt_PT")
+			current_language = "pt_PT"
+		1: #English
+			TranslationServer.set_locale("en")
+			current_language = "en"
+		2: #日本語
+			TranslationServer.set_locale("ja")
+			current_language = "ja"
