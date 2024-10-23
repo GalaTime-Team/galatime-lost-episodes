7213244bb3a65d91982476a15bac661a56ef4ddf
Nihhiu <106330016+Nihhiu@users.noreply.github.com>
2024-10-23 01:15:05 +0100

    resolver os problemas do Artur
    ele ainda tem mentais: irresolvivel

--- Script/splash_screen.gd
+++ Script/splash_screen.gd
@@ -8,23 +8,16 @@ extends Control
 @export var out_time : float = 1.0
 @export var splash_screen : ColorRect
 
-@export var splash1 : ColorRect
-@export var splash2 : TextureRect
-
 func _ready() -> void:
 	fade()
 
 func fade() -> void:
 	splash_screen.modulate.a = 1.0
-	splash1.modulate.a = 1.0
-	splash2.modulate.a = 1.0
 	var tween = self.create_tween()
 	tween.tween_interval(in_time) #intervalos
 	tween.tween_property(splash_screen, "modulate:a", 0.0 ,fade_in_time) #fazer aparecer a imagem
 	tween.tween_interval(pause_time) #intervalos
 	tween.tween_property(splash_screen, "modulate:a", 1.0 , fade_out_time) #fazer transparecer a imgem
 	tween.tween_interval(out_time) #intervalos
-	tween.tween_property(splash1, "modulate:a", 0.0 ,0.0)
-	tween.tween_property(splash2, "modulate:a", 0.0 ,0.0)
 	await tween.finished
 	get_tree().change_scene_to_packed(load_scene)
