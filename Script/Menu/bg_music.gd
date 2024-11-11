f034576c7727cd34f67c3e36170ab4666d9ff7dc
Mr-Sann <139286702+Mr-Sann@users.noreply.github.com>
2024-11-11 23:35:31 +0000

    versão 0.2.0 concluida
    acho eu kk

--- Script/Menu/bg_music.gd
+++ Script/Menu/bg_music.gd
@@ -9,17 +9,13 @@ func _ready() -> void:
 	play_music()
 	pass # Replace with function body.
 
-
-
 # Fução para dar play na música com delay aleatório
 func play_music():
 	play()
-	
 
 # Função para quando acaba o timer
 func _on_Timer_timeout():
 	play_music()
-
 
 func _on_finished() -> void:
 	var delay = randi() % 20 + 1  #delay aleatório entre as músicas
--- Script/intro_monologue.gd
+++ Script/intro_monologue.gd
@@ -1,7 +1,8 @@
 class_name IntroMonologue
 extends Control
-@export var fogo: AudioStreamPlayer 
-@export var chuva: AudioStreamPlayer 
+@export var fogo : AudioStreamPlayer 
+@export var chuva : AudioStreamPlayer 
+@export var fundo : AudioStreamPlayer
 
 @export var margin_container: MarginContainer
 var intromonologue = "intromonologue"
@@ -32,8 +33,6 @@ func start_monologue():
 	Dialogic.signal_event.connect(end_of_dialog)
 	Dialogic.start(intromonologue)
 
-
-
 func end_of_dialog(argument : String):
 	if argument == "end_of_dialogue":
 		set_process(false)
@@ -41,14 +40,15 @@ func end_of_dialog(argument : String):
 		var tween = self.create_tween()
 		tween.tween_property(chuva, "volume_db", -80, 2.0)
 		tween.tween_property(fogo, "volume_db", -80, 2.0)
+		tween.tween_property(fundo , "volume_db", -80, 2.0)
 		await tween.finished
 		tween.stop()
 		fogo.stop()
 		chuva.stop()
+		fundo.stop()
 	elif argument == "fogo":
 		var tween = self.create_tween()
 		tween.tween_property(chuva, "volume_db", -7, 1.0)
-		
 		fogo.volume_db = -80
 		fogo.play()
 		tween.tween_property(fogo, "volume_db", -11, 2.0)
