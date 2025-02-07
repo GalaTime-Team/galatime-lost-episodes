be002d88cecf93fc44f0665347cfeed91b655348
MatheusCBAbreu <edoardoqlike@gmail.com>
2025-02-07 13:50:32 +0000

    Back Ground Music do Jogo
    Não sei se ta mas deve tar certo

---
+++ Interface/Play/bg_jogo.gd
@@ -0,0 +1,22 @@
+extends AudioStreamPlayer
+
+@export var timer: Timer 
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	timer.timeout.connect(_on_Timer_timeout)
+	play_music()
+
+# Fução para dar play na música com delay aleatório
+func play_music():
+	play()
+
+# Função para quando acaba o timer
+func _on_Timer_timeout():
+	play_music()
+
+func _on_finished() -> void:
+	var delay = randi() % 20 + 1  #delay aleatório entre as músicas
+	timer.wait_time = delay
+	timer.start()
